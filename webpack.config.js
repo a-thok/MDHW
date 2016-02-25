6933e51c64ad766ae7df7a50eb7eb52aa01a2e96
Victor <a-thok@outlook.com>
2016-02-25 09:13:37 +0800

    chore: first commit

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: {
+    'rczp': [__dirname + '/src/js/rczp.js']
+  },
+  output: {
+    path: __dirname + '/dist/',
+    filename: 'js/[name].js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['', '.js', '.json', '.css']
+  },
+  externals: [],
+  module: {
+    //加载器配置
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style', 'css!postcss')
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.(woff|svg|eot|ttf)\??.*$/,
+        loader: 'url?limit=10000&name=font/[name].[ext]?[hash]'
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'url?limit=8192&name=img/[name].[ext]?[hash]'
+      },
+      // {
+      //   test: require.resolve('angular'),
+      //   loader: 'expose?angular'
+      // },
+    ]
+  },
+  plugins: [
+    // new webpack.optimize.CommonsChunkPlugin('js/common.js'),
+    new webpack.HotModuleReplacementPlugin(),
+    new ExtractTextPlugin('css/[name].css')
+  ],
+  postcss: function(webpack) {
+    return [
+      require('postcss-import')({
+        addDependencyTo: webpack
+      }),
+      require('postcss-opacity'),
+      require('postcss-cssnext')()
+    ]
+  }
+};
