5a833ace1c906ef11523225cbc5db3b4a1694ecf
Victor <a-thok@outlook.com>
2016-03-01 10:39:23 +0800

    chore: 引入fastclick

--- src/rczp/rczp.js
+++ src/rczp/rczp.js
@@ -1,5 +1,9 @@
 import 'font-awesome/css/font-awesome.css';
 import './css/main.css';
 
+// 解决click事件的移动端延迟
+import fastclick from 'fastclick';
+fastclick.attach(document.body);
+
 import company from './js/company.js';
 company();
