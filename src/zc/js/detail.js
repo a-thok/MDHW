50e8e2ae45189027970e83152b5acf56abea14af
jixu <724921588@qq.com>
2016-03-03 18:06:12 +0800

    feat: zc - 分享框弹出取消

---
+++ src/zc/js/detail.js
@@ -0,0 +1,10 @@
+export default function detail () {
+  document.querySelector('.dtImg_btn').addEventListener('click', () => {
+    let share = document.querySelector('.share')
+    share.style.display = 'block'
+  })
+  document.querySelector('.share_cancel').addEventListener('click', (el) => {
+    let share = document.querySelector('.share')
+    share.style.display = 'none'
+  })
+}
---
+++ src/zc/zc.detail.js
@@ -0,0 +1,5 @@
+import 'font-awesome/css/font-awesome.css'
+import './css/main.css'
+
+import detail from './js/detail.js'
+detail()
