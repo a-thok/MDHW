4bf6097531ea3eadbf37ea65acc711cba0c491eb
Victor <a-thok@outlook.com>
2016-03-04 10:06:52 +0800

    调整字体打包

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,11 @@ module.exports = {
       },
       {
         test: /\.(woff|svg|eot|ttf)\??.*$/,
-        loader: 'url?limit=10000&name=font/[name].[ext]?[hash]'
+        loader: 'url',
+        query: {
+          limit: 10000,
+          name: 'font/[name].[ext]?[hash:7]'
+        }
       },
       {
         test: /\.(png|jpg|gif)$/,
