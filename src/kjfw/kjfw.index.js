b528c1c72df52fadf4be3bd8c6c181f248c533d9
Victor <a-thok@outlook.com>
2016-03-05 16:40:02 +0800

    删除临时文件

--- src/kjfw/kjfw.index.js
+++ src/kjfw/kjfw.index.js
@@ -1,9 +1,2 @@
 import 'font-awesome/css/font-awesome.css'
 import './css/main.css'
-
-// 解决click事件的移动端延迟
-import fastclick from 'fastclick'
-fastclick.attach(document.body)
-
-import slider from './js/slider.js'
-slider()
