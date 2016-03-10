1006d87427fe531f6c0ccae0e66ccad3458c6cf6
lqt <1131763154@qq.com>
2016-03-10 16:40:46 +0800

    众筹 评论页动态化

---
+++ src/zc/js/commentList.js
@@ -0,0 +1,47 @@
+import render from '../../common/js/render.js'
+
+export default function commentList() {
+  let template = function (data) {
+    return data.result.data.reduce((pre, cur) => {
+      let fuli = cur.fuli.reduce((_pre, _cur) => {
+        return _pre + ` <li class="dialog_sublist_item">
+              <div class="dialog_sublist_item_username">${_cur.user}</div>
+              <div class="dialog_sublist_item_comment">
+                <i class="dialog_sublist_item_arrow"></i>
+                <p class="dialog_sublist_item_content">${_cur.comment}</p>
+                <p class="dialog_list_item_info_time"><time>${_cur.date}</time></p>
+              </div>
+            </li>`
+      }, '')
+
+      return pre + ` <li class="dialog_list_item">
+          <div class="dialog_list_item_user">
+            <div class="dialog_list_item_user_avatar"></div><!--这里应该是一张图片-->
+            <span class="dialog_list_item_user_name">${cur.user}</span>
+          </div>
+          <p class="dialog_list_item_content">${cur.comment}</p>
+          <div class="dialog_list_item_info">
+            <time class="dialog_list_item_info_time"><i class="fa fa-clock-o"></i>${cur.date}</time>
+            <button class="dialog_list_item_info_btn" type="button"><i class="fa fa-commenting-o"></i>评论</button>
+          </div>
+          <!--子评论 开始-->
+          <ul class="dialog_sublist">
+            <li class="dialog_sublist_arrow"></li>
+            ${fuli}
+          </ul>
+        </li>`
+    }, '')
+  }
+  let button = document.querySelector('.dialog_more_link')
+  let config = {
+    api: '/m/ZC/Comments',
+    body: {
+      fpid: 1,
+      pageIndex: 1,
+      pageSize: 10
+    },
+    template: template,
+    container: document.querySelector('.dialog_list')
+  }
+  render(button, config)
+}
