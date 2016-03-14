978a68ff74daebecadbc68e6eca62e20c2004bd2
jixu <724921588@qq.com>
2016-03-14 13:37:46 +0800

    众筹分享模块

--- src/zc/js/detail.js
+++ src/zc/js/detail.js
@@ -1,10 +1,13 @@
 export default function detail() {
-  document.querySelector('.dtImg_btn').addEventListener('click', () => {
-    let share = document.querySelector('.share')
-    share.style.display = 'block'
-  })
-  document.querySelector('.share_cancel').addEventListener('click', (el) => {
-    let share = document.querySelector('.share')
+  let shareArry = [document.querySelector('.header_login'), document.querySelector('.dtImg_btn')]
+  let share = document.querySelector('.sliderShare')
+  for (let i = 0, len = shareArry.length; i < len; i++) {
+    shareArry[i].addEventListener('click', () => {
+      console.log(i)
+      share.style.display = 'block'
+    })
+  }
+  document.querySelector('.sliderShare_box_cancel').addEventListener('click', () => {
     share.style.display = 'none'
   })
 }
