7b0fc0c048a255d97682a786f5e164601333ba0c
jixu <724921588@qq.com>
2016-03-25 15:41:53 +0800

    众筹搜索功能优化

--- src/zc/js/category.js
+++ src/zc/js/category.js
@@ -1,23 +1,18 @@
 import render from 'render'
 import { fixFilter, showFilter, selectFilter } from 'filter'
 import template from './template.js'
+import { $ } from 'func'
 
 export default function category() {
   // 过滤
   fixFilter()
   showFilter()
   selectFilter()
-  
   // 渲染列表
-  let button = document.querySelector('.list_more')
-  let config = {
+  render({
+    template,
+    buttons: $('.list_more'),
     api: '/m/ZC/ZcList',
-    body: {
-      pageIndex: 1,
-      pageSize: 10
-    },
-    template: template,
-    container: document.querySelector('.zcList')
-  }
-  render(button, config)
+    container: $('.zcList')
+  })
 }
