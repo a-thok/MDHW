9534db4b2c030b8bf567b881d3e52b0eb6c8a431
lqt <1131763154@qq.com>
2016-03-25 15:36:04 +0800

    私人定制搜索框

--- src/srdz/js/search.js
+++ src/srdz/js/search.js
@@ -1,45 +1,37 @@
-import { $from } from 'func'
+import { $, pageCallback } from 'func'
+import doSearch from 'doSearch'
+import template from './template.js'
 // import render from 'render'
 
 // 搜索切换
-export function search() {
-  $from('.srchBtn_list_item').forEach(el => {
-    el.addEventListener('click', e => {
-      if (e.target.classList.contains('srchBtn_list_item-bgColor')) return
-      $from(e.target.parentElement.children).forEach((_e, _index) => {
-        _e.classList.remove('srchBtn_list_item-bgColor')
-      })
-      e.target.classList.add('srchBtn_list_item-bgColor')
-    })
+export default function search() {
+  // $from('.srchBtn_list_item').forEach(el => {
+  //   el.addEventListener('click', e => {
+  //     if (e.target.classList.contains('srchBtn_list_item-bgColor')) return
+  //     $from(e.target.parentElement.children).forEach((_e, _index) => {
+  //       _e.classList.remove('srchBtn_list_item-bgColor')
+  //     })
+  //     e.target.classList.add('srchBtn_list_item-bgColor')
+  //   })
+  // })
+  
+  let config = {
+    template,
+    buttons: $('.pagination_btn'),
+    api: '/m/Srdz/SrdzList',
+    container: document.querySelector('.hostlist'),
+    cb: pageCallback
+  }
+  
+  doSearch({
+    config,
+    srchbtn: '.srch_btn'
+    // url: () => {
+    //   let searchText = $('.srchBtn_list')
+    //   let type = searchText.getAttributeNode('data-type').value
+    //   return (+type === 1) ? '/m/HR/JobList' : '/m/HR/CompanyList'
+    // }
   })
 }
-// 搜索
-// export function searchResult() {
-//   let button = document.querySelector('.srch_btn')
-//   let keyword = $('#search').value.trim()
-//   let config = {
-//     api: '',
-//     body: {
-//       pageIndex: 0,
-//       pageSize: 10,
-//       keyword: keyword
-//     },
-//     immediate: true
-//   }
-//   if (keyword.length === 0) return
-//   Object.assign(config, {
-//     body: {
-//       pageIndex: 0,
-//       pageSize: 10,
-//       keyword: keyword
-//     },
-//     immediate: true
-//   })
-//   render(button, config)
-// }
-// $('#search').addEventListener('keyup', e => {
-//   console.log(1)
-//   if (e.keyCode === 13) searchResult()
-// })
-// $('.srch_btn').addEventListener('click', () => searchResult())
+
 
