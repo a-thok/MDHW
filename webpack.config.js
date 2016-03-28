ef45411e6536fe0b7bbc6d6f250a8d271e109603
Victor <a-thok@outlook.com>
2016-03-28 15:46:37 +0800

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
+'use strict';
+
 const path = require('path');
 const webpack = require('webpack');
+const CleanPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const PRODUCTION = process.env.NODE_ENV === 'production';
 
-const BASE_CSS_PATH = path.join(__dirname, './src/common/css/base.css');
-const PATH_ARRAY = ['func', 'render', 'filter', 'slider', 'doSearch']
-  .map((item) => path.join(__dirname, `./src/common/js/${item}.js`));
-
-module.exports = {
+const config = {
   entry: {
-    'common': ['font-awesome/css/font-awesome.css', BASE_CSS_PATH, 'es6-promise', 'whatwg-fetch', 'fastclick'].concat(PATH_ARRAY),
+    'common': ['font-awesome/css/font-awesome.css', 'es6-promise', 'whatwg-fetch', 'fastclick'],
     // 'rczp': [path.join(__dirname, 'src/rczp/rczp.js')],
     // 'kjfw': [path.join(__dirname, 'src/kjfw/kjfw.js')],
     // 'zc': [path.join(__dirname, 'src/zc/zc.js')],
@@ -20,10 +21,11 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, '/dist/'),
+    publicPath: PRODUCTION ? '//cdn.dreamhiway.com/static' : '/',
     filename: 'js/[name].js'
   },
   resolve: {
-    extensions: ['', '.js', '.json', '.css'],
+    extensions: ['', '.js', 'jsx', '.json', '.css'],
     root: path.resolve('./src/common/js'),
     alias: {
       'func': 'func.js',
@@ -33,20 +35,21 @@ module.exports = {
       'doSearch': 'doSearch.js'
     }
   },
+  devtool: PRODUCTION ? 'source-map' : 'eval-source-map',
   module: {
     loaders: [
       {
         test: /\.css$/,
-        loader: 'style!css?sourceMap!postcss'
+        loader: PRODUCTION ? ExtractTextPlugin.extract('style', 'css!postcss') : 'style!css?sourceMap!postcss'
       },
       {
         test: /\.jsx?$/,
-        loader: 'babel!eslint',
+        loader: PRODUCTION ? 'babel' : 'babel!eslint',
         exclude: path.resolve(__dirname, 'node_modules')
       },
       {
         test: /\.(woff|svg|eot|ttf)\??.*$/,
-        loader: 'url?sourceMap',
+        loader: PRODUCTION ? 'url' : 'url?sourceMap',
         query: {
           limit: 10000,
           name: 'font/[name].[ext]?[hash:7]'
@@ -66,7 +69,12 @@ module.exports = {
     new webpack.optimize.CommonsChunkPlugin('common', 'js/common.js', Infinity)
   ],
   postcss: function (webpack) {
-    return [
+    return PRODUCTION ? [
+      require('postcss-import')({
+        addDependencyTo: webpack
+      }),
+      require('postcss-cssnext')()
+    ] : [
       require('postcss-import')({
         addDependencyTo: webpack
       }),
@@ -79,3 +87,47 @@ module.exports = {
     ];
   }
 };
+
+// add shared js and css to common chunck
+const BASE_CSS_PATH = path.join(__dirname, './src/common/css/base.css');
+const PATH_ARRAY = ['func', 'render', 'filter', 'slider', 'doSearch']
+  .map((item) => path.join(__dirname, `./src/common/js/${item}.js`));
+config.entry.common.push(BASE_CSS_PATH);
+config.entry.common = config.entry.common.concat(PATH_ARRAY);
+
+// add different plugins for production and development
+let extraPlugins;
+if (PRODUCTION) {
+  extraPlugins = [
+    new CleanPlugin('dist'),
+    new ExtractTextPlugin('css/[name].css'),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      __SERVER__: !PRODUCTION,
+      __DEVELOPMENT__: !PRODUCTION,
+      __DEVTOOLS__: !PRODUCTION,
+      'process.env': {
+        BABEL_ENV: JSON.stringify(process.env.NODE_ENV)
+      }
+    })
+  ];
+} else {
+  const hotClient = 'webpack-hot-middleware/client?noInfo=true&reload=true';
+  Object.keys(config.entry).forEach((name) => {
+    config.entry[name] = [hotClient].concat(config.entry[name]);
+  });
+
+  extraPlugins = [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ];
+}
+config.plugins = config.plugins.concat(extraPlugins);
+
+module.exports = config;
