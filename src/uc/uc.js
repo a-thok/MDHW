fa3cc136e1e793eb4c78c94e197717139554a46b
Victor <a-thok@outlook.com>
2016-03-29 16:44:01 +0800

    个人中心 地址列表

--- src/uc/uc.js
+++ src/uc/uc.js
@@ -5,12 +5,14 @@ import { Router, Route, hashHistory } from 'react-router';
 import Home from './routes/Home';
 import Reservation from './routes/Reservation';
 import ReservationImfor from './routes/ReservationImfor';
+import Addresses from './routes/Addresses';
 import App from './App';
 
 ReactDOM.render((
   <Router history={hashHistory}>
     <Route path="/" component={App}>
       <Route path="/home" component={Home} />
+      <Route path="/address" component={Addresses} />
       <Route path="/reservation" component={Reservation} />
       <Route path="/reservationImfor" component={ReservationImfor} />
     </Route>
