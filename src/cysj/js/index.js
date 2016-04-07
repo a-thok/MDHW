8e23563a6a0219f73ddc92f6ca3b7d52783d51b3
jixu <724921588@qq.com>
2016-04-07 11:40:48 +0800

    创意设计首页搜索JS添加

--- src/cysj/js/index.js
+++ src/cysj/js/index.js
@@ -1,5 +1,20 @@
+import { $ } from 'func';
 import slider from 'slider';
 
 export default function index() {
   slider(document.querySelector('.sliderBox'));
+  function search() {
+    const keyword = $('.srch_input').value.trim();
+    let url = `http://192.168.2.177:8092/m/cysj/search?kw=${keyword}`;
+    location.href = url;
+  }
+  $('#search').addEventListener('keyup', e => {
+    if (e.keyCode === 13) {
+      search();
+    }
+  });
+  let btn = $('.srch_btn');
+  btn.addEventListener('click', () => {
+    search();
+  });
 }
