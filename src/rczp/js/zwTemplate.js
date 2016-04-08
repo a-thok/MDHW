5fb3e56745a0a516283acb41a4ad3b3985189d66
lqt <1131763154@qq.com>
2016-04-08 15:01:06 +0800

    职位详情页

--- src/rczp/js/zwTemplate.js
+++ src/rczp/js/zwTemplate.js
@@ -4,7 +4,7 @@ export default function zwTemplate(data) {
           <div class="list_item_img"><img src="http://192.168.2.10:82/img/${cur.logo}"></div>
           <div class="list_item_text">
             <p class="list_item_text_p post_info">
-              <a href="http://192.168.2.177:8086/m/hr/detail/${cur.id}"><span class="post_name list_margin">${cur.Position}</span></a>
+              <a href="http://192.168.2.177:8086/m/hr/detail/${cur.ID}"><span class="post_name list_margin">${cur.Position}</span></a>
               [${cur.CityName}] ${cur.PublicTime}
             </p>
             <p class="list_item_text_p post_info post_info-color"><span class="post_salary list_margin">月薪: ${cur.Money}</span>经验: ${cur.gznx}</p>
