f2cffef14cb0eac82fb92e0f647ccfa8554f78b6
jixu <724921588@qq.com>
2016-04-08 20:35:35 +0800

    会计服务优化提价后台动态化

---
+++ src/kjfw/js/template.js
@@ -0,0 +1,43 @@
+export default function template(data) {
+  return data.result.data.reduce((pre, cur) => {
+    const business = cur.business.reduce((_pre, _cur) => `${_pre}<li class="tagList_item">${_cur.name}</li>`, '');
+    let url = cur.log ? `http://192.168.2.10:82/img/${cur.logo}` : 'http://192.168.2.10:81/images/default2.png';
+    return (
+      `${pre}<ul class="list">
+        <li class="list_item">
+          <div class="list_item_title">
+            <i class="fa fa-heart-o"></i>${cur.servertenet}
+          </div>
+          <!--公司信息  开始-->
+          <div class="list_item_content">
+            <h3 class="list_item_content_title">
+              <a class="list_link link-color" href="#" alt="福州市圣鹏财务顾问有限公司">${cur.company}</a>
+            </h3>
+            <div class="list_item_content_info">
+              <div class="list_item_content_info_left">
+                <img src="${url}" alt="${cur.logo}">
+                <span class="list_item_phone"><i class="fa fa-phone"></i>${cur.phone}</span>
+              </div>
+              <div class="list_item_content_info_right">
+                <ul class="iconList">
+                  <li class="iconList_item"><i class="fa fa-shield"></i></li>
+                  <li class="iconList_item"><i class="fa fa-sort-amount-desc"></i></li>
+                  <li class="iconList_item"><i class="fa fa-car"></i></li>
+                  <li class="iconList_item"><i class="fa fa-picture-o"></i></li>
+                </ul>
+                <ul class="tagList tagList-kjfw">
+                  ${business}
+                </ul>
+              </div>
+            </div>
+            <p class="list_item_content_text">
+              ${cur.prof}
+              <span>>></span>
+            </p>
+          </div>
+          <!--公司信息  结束-->
+          <div class="list_item_detail">查看公司详情</div>
+        </li>
+      </ul>`);
+  }, '');
+}
--- src/kjfw/kjfw.js
+++ src/kjfw/kjfw.js
@@ -5,6 +5,13 @@ import fastclick from 'fastclick';
 fastclick.attach(document.body);
 
 import assess from './js/assess.js';
+import index from './js/index.js';
+import company from './js/company.js';
+
 if (window.location.pathname.indexOf('assess') !== -1) {
   assess();
+} else if (window.location.pathname.indexOf('index') !== -1) {
+  index();
+} else if (window.location.pathname.indexOf('company') !== -1) {
+  company();
 }
