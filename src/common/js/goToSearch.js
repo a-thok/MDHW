948d4d04a06f0f2391171c6f1aab3161d1c2a4f6
Victor <a-thok@outlook.com>
2016-04-09 09:16:52 +0800

    转跳搜索页方法

---
+++ src/common/js/goToSearch.js
@@ -0,0 +1,16 @@
+export default function goToSearch({ input, partialUrl, btn }) {
+  function search() {
+    const keyword = input.value.trim();
+    if (!keyword) return;
+    let url = `${partialUrl}${keyword}`;
+    location.href = url;
+  }
+  input.addEventListener('keyup', e => {
+    if (e.keyCode === 13) {
+      search();
+    }
+  });
+  btn.addEventListener('click', () => {
+    search();
+  });
+}
