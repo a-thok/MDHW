444e3ba11a56f7c885023df661404c0aed3f5ce5
jixu <724921588@qq.com>
2016-04-09 17:32:04 +0800

    合并

--- src/zc/index.js
+++ src/zc/index.js
@@ -4,24 +4,27 @@ import './css/main.css';
 import fastclick from 'fastclick';
 fastclick.attach(document.body);
 
+import showMenu from 'showMenu';
 import detail from './js/detail.js';
-
 import category from './js/category.js';
 import comment from './js/comment.js';
 import progress from './js/progress.js';
 import search from './js/search.js';
 import index from './js/index.js';
 
+const pathname = window.location.pathname;
 if (document.location.pathname.indexOf('detail') !== -1) {
   detail();
+  progress();
 } else if (document.location.pathname.indexOf('category') !== -1) {
   category();
 } else if (document.location.pathname.indexOf('comment') !== -1) {
   comment();
 } else if (document.location.pathname.indexOf('progress') !== -1) {
   progress();
+  showMenu();
 } else if (document.location.pathname.indexOf('search') !== -1) {
   search();
-} else if (document.location.pathname.indexOf('index') !== -1) {
+} else if (document.location.pathname.indexOf('index') !== -1 || pathname === '/m/zc') {
   index();
 }
