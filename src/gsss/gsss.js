90ba9fac622dfcf8a0a613b8020ebbe30671465a
lqt <1131763154@qq.com>
2016-04-09 09:15:31 +0800

    公司上市

--- src/gsss/gsss.js
+++ src/gsss/gsss.js
@@ -1,1 +1,15 @@
 import './css/main.css';
+
+import index from './js/index.js';
+
+import './img/gssslogolqt.png';
+import './img/1-banner.png';
+import './img/iconfont-chengweitouziren2.png';
+import './img/iconfont-gongsi.png';
+import './img/hotlqt.png';
+
+const pathname = window.location.pathname;
+
+if (pathname.indexOf('index') !== -1) {
+  index();
+}
