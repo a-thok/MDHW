64f1700f3855bc618ae4e7abfb296dfd50f068c2
lqt <1131763154@qq.com>
2016-04-13 11:28:11 +0800

    众创

--- src/home/js/login.js
+++ src/home/js/login.js
@@ -16,62 +16,63 @@ export default function login() {
     e.preventDefault();
     login.disabled = true;
 
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          const res = JSON.parse(xhr.response);
-          if (res.success) {
-            let locationSrch = location.search;
-            let urlEndPoint = locationSrch.indexOf('&');
-            let url = locationSrch.slice(13, locationSrch.length - urlEndPoint);
-            // console.log(decodeURIComponent(url));
-            location.href = decodeURIComponent(url);
-          } else {
-            if (res.msg) {
-              modalText.textContent = res.msg;
-            } else {
-              modalText.textContent = '未知错误，请稍候重试';
-            }
-            refresh();
-            modal.classList.add('is-show');
-            setTimeout(() => modal.classList.remove('is-show'), 2500);
-          }
-        }
-      }
-    };
-
-    xhr.open('POST', '/m/main/Login', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify(data));
-
-    // fetch('/m/main/Login', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   },
-    //   body: JSON.stringify(data)
-    // })
-    //   .then(res => res.json())
-    //   .then(res => {
-    //     if (res.success) {
-    //       let locationSrch = location.search;
-    //       let urlEndPoint = locationSrch.indexOf('&');
-    //       let url = locationSrch.slice(13, locationSrch.length - urlEndPoint);
-    //       // console.log(decodeURIComponent(url));
-    //       location.href = decodeURIComponent(url);
-    //       // alert('登录成功');
-    //     } else {
-    //       if (res.msg) {
-    //         modalText.textContent = res.msg;
+    // const xhr = new XMLHttpRequest();
+    // xhr.onreadystatechange = function () {
+    //   if (xhr.readyState === 4) {
+    //     if (xhr.status === 200) {
+    //       const res = JSON.parse(xhr.response);
+    //       if (res.success) {
+    //         let locationSrch = location.search;
+    //         let urlEndPoint = locationSrch.indexOf('&');
+    //         let url = locationSrch.slice(13, locationSrch.length - urlEndPoint);
+    //         // console.log(decodeURIComponent(url));
+    //         location.href = decodeURIComponent(url);
     //       } else {
-    //         modalText.textContent = '未知错误，请稍候重试';
+    //         if (res.msg) {
+    //           modalText.textContent = res.msg;
+    //         } else {
+    //           modalText.textContent = '未知错误，请稍候重试';
+    //         }
+    //         refresh();
+    //         modal.classList.add('is-show');
+    //         setTimeout(() => modal.classList.remove('is-show'), 2500);
     //       }
-    //       refresh();
-    //       modal.classList.add('is-show');
-    //       setTimeout(() => modal.classList.remove('is-show'), 2500);
     //     }
-    //   });
+    //   }
+    // };
+
+    // xhr.open('POST', '/m/main/Login', true);
+    // xhr.setRequestHeader('Content-Type', 'application/json');
+    // xhr.send(JSON.stringify(data));
+
+    fetch('/m/main/Login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'credentials': 'include'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(res => {
+        if (res.success) {
+          let locationSrch = location.search;
+          let urlEndPoint = locationSrch.indexOf('&');
+          let url = locationSrch.slice(13, locationSrch.length - urlEndPoint);
+          // console.log(decodeURIComponent(url));
+          location.href = decodeURIComponent(url);
+          // alert('登录成功');
+        } else {
+          if (res.msg) {
+            modalText.textContent = res.msg;
+          } else {
+            modalText.textContent = '未知错误，请稍候重试';
+          }
+          refresh();
+          modal.classList.add('is-show');
+          setTimeout(() => modal.classList.remove('is-show'), 2500);
+        }
+      });
   }
 
   login.addEventListener('click', doLogin);
--- src/zckj/index.js
+++ src/zckj/index.js
@@ -1,8 +1,11 @@
 // import 'font-awesome/css/font-awesome.css'
 import './css/main.css';
 
-import './img/1-banner.png';
+import './img/body.png';
 import './img/zckj_logo.png';
+import './img/lqt.png';
+import './img/iconfont-qiyerenzheng1.png';
+import './img/iconfont-qiyerenzheng.png';
 // 解决click事件的移动端延迟
 import fastclick from 'fastclick';
 fastclick.attach(document.body);
@@ -15,8 +18,8 @@ const pathname = window.location.pathname;
 
 if (pathname.indexOf('detail') !== -1) {
   detail();
-} else if (pathname.indexOf('index') !== -1) {
+} else if (pathname.indexOf('index') !== -1 || pathname === '/m/zckj') {
   index();
-} else if (pathname.indexOf('list') !== -1 || pathname === '/m/zckj') {
+} else if (pathname.indexOf('list') !== -1) {
   list();
 }
