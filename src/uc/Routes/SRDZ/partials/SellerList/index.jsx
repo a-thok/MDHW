cc1358ec5851edfa7b9145992e6d8e7c8327404f
jixu <724921588@qq.com>
2016-04-15 13:48:28 +0800

    合并

--- src/uc/Routes/SRDZ/partials/SellerList/index.jsx
+++ src/uc/Routes/SRDZ/partials/SellerList/index.jsx
@@ -1,39 +1,28 @@
 import React from 'react';
 import ListItem from '../../../../components/ListItem';
+import ListItemDetail from '../../../../components/ListItemDetail';
 
-export default React.createClass({
-  getInitialState: function () {
-    let res = {
-      data: [
-      { img: '图1', title: '蓝牙耳机——世界上最小的立体声无线耳机', buyerName: '张三', state: '待发货', type: '科学' },
-      { img: '图1', title: '蓝牙耳机——世界上最小的立体声无线耳机', buyerName: '李四', state: '待发货', type: '日常' },
-      { img: '图1', title: '蓝牙耳机——世界上最小的立体声无线耳机', buyerName: '马武', state: '待发货', type: '科学' },
-      { img: '图1', title: '蓝牙耳机——世界上最小的立体声无线耳机', buyerName: '午马', state: '待发货', type: '科学' }
-      ]
-    };
-    return res;
-  },
-  render: function () {
-    const content = this.state.data.map((item, index) => (
-      <ListItem
-        key={index}
-        {...item}
-        multiple={{ '状态': item.state }}
-        emp={['买家名称', item.buyerName]}
-        small={item.type}
-        tep="down"
-      />
-    ));
-    return (
-      <div>
-        <section style={{ display: 'flex', justifyContent: 'space-around' }}>
-          <p>待付款</p>
-          <p>已付款</p>
-        </section>
-        <ul className="list">
-          {content}
-        </ul>
-      </div>
-    );
-  }
-});
+export default function SellerList(props) {
+  const content = props.data.map((item, index) => (
+    <ListItem
+      key={index}
+      {...item}
+      multiple={{ '状态': item.state }}
+      emp={['买家名称', item.buyerName]}
+      small={item.type}
+      tep="down"
+      extra={<ListItemDetail />}
+    />
+  ));
+  return (
+    <div>
+      <section style={{ display: 'flex', justifyContent: 'space-around' }}>
+        <p>待付款</p>
+        <p>已付款</p>
+      </section>
+      <ul className="list">
+        {content}
+      </ul>
+    </div>
+  );
+}
