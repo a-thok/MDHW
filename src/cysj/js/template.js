87ceacc599d2110204d628c8df74e10661236681
jixu <724921588@qq.com>
2016-04-16 11:37:49 +0800

    创意设计优化

--- src/cysj/js/template.js
+++ src/cysj/js/template.js
@@ -1,9 +1,11 @@
 export default function template(data) {
-  return data.result.data.reduce((pre, cur) => (
+  return data.result.data.reduce((pre, cur) => {
+    let url = cur.logo ? `http://192.168.2.10:82/img/${cur.logo}` : 'http://cdn.dreamhiway.com/images/default2.png';
+    return (
     `${pre}<li class="hostlist_item">
       <a class="hostlist_item_a" href="http://192.168.2.177:8092/m/diy/detail/${cur.id}">
         <div class="hostlist_item_img">
-          <img src="http://192.168.2.10:82/img/${cur.logo}">
+          <img src="${url}">
         </div>
         <div class="hostlist_content">
           <div class="base_title_wrap">
@@ -21,5 +23,6 @@ export default function template(data) {
         </div>
       </a>
     </li>`
-  ), '');
+    );
+  }, '');
 }
