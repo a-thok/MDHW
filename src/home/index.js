ba13bf713d34f15aad3cda3a568243693a20c540
jixu <724921588@qq.com>
2016-04-17 11:42:32 +0800

    首页 热门 滚动

--- src/home/index.js
+++ src/home/index.js
@@ -2,7 +2,7 @@ import './css/main.css';
 
 import login from './js/login.js';
 import register from './js/register.js';
-import index from './js/index.js';
+import { index, scroll } from './js/index.js';
 
 const pathname = window.location.pathname;
 if (pathname.indexOf('denglu') !== -1) {
@@ -10,5 +10,6 @@ if (pathname.indexOf('denglu') !== -1) {
 } else if (pathname.indexOf('register') !== -1) {
   register();
 } else if (pathname.indexOf('index') !== -1 || pathname === '/m/home') {
+  scroll();
   index();
 }
--- src/home/js/index.js
+++ src/home/js/index.js
@@ -2,6 +2,19 @@ import slider from 'slider';
 // import { $ } from 'func';
 
 
-export default function index() {
+export function index() {
   slider(document.querySelector('.sliderBox'));
+}
+
+export function scroll() {
+  let scrollBox = document.querySelector('.topLine_filterBox_scroll');
+  let margintop = scrollBox.style.marginTop;
+  function time() {
+    if (-margintop > scrollBox.clientHeight - 6) {
+      margintop = 0;
+    }
+    margintop -= 1;
+    scrollBox.style.marginTop = `${margintop}px`;
+  }
+  scrollBox.timer = setInterval(() => time(), 100);
 }
