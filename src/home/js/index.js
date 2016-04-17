19b97a329ab7f13ebf5c141d44730cf1d2cd6282
jixu <724921588@qq.com>
2016-04-17 19:46:49 +0800

    首页 滚动优化

--- src/home/js/index.js
+++ src/home/js/index.js
@@ -1,19 +1,16 @@
 import slider from 'slider';
-import { $ } from 'func';
+// import { $ } from 'func';
 
 
 export function index() {
-  slider($('.sliderBox'));
+  slider(document.querySelector('.sliderBox'));
 }
 
 export function scroll() {
-  // 先复制一份需要滚动的html 代码
-  let parent = $('.topLine_filterBox');
-  parent.insertAdjacentHTML('beforeend', parent.innerHTML);
-
-  let scrollBox = $('.topLine_filterBox_scroll');
+  let filterBox = document.querySelector('.topLine_filterBox');
+  let scrollBox = document.querySelector('.topLine_filterBox_scroll');
   let margintop = scrollBox.style.marginTop;
-
+  filterBox.insertAdjacentHTML('beforeend', filterBox.innerHTML);
   function time() {
     if (-margintop > scrollBox.clientHeight - 1) {
       margintop = 0;
