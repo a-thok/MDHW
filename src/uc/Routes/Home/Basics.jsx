261be33fe02629918e39a4e2af53e2dcf56f9325
lqt <1131763154@qq.com>
2016-04-17 10:29:30 +0800

    个人中心首页加图片

--- src/uc/Routes/Home/Basics.jsx
+++ src/uc/Routes/Home/Basics.jsx
@@ -2,34 +2,23 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default function Basics(props) {
-  let liStyle = {
-    float: 'left',
-    width: '33.3333%',
-    textAlign: 'center',
-    height: '100%'
-  };
   return (
-    <ul
-      className="basics"
-      style={{
-        overflow: 'hidden'
-      }}
-    >
-      <li style={liStyle}>
+    <ul className="basics" >
+      <li className="basics_item">
         <Link to="/">
           <section><i className="fa fa-commenting-o"></i></section>
           <section>消息</section>
           <section className="basics_num">{props.msg}</section>
         </Link>
       </li>
-      <li style={liStyle}>
+      <li className="basics_item">
         <Link to="/">
           <section><i className="fa fa-user"></i></section>
           <section>好友</section>
           <section className="basics_num">{props.frd}</section>
         </Link>
       </li>
-      <li style={liStyle}>
+      <li className="basics_item">
         <Link to="/">
           <section><i className="fa fa-rmb"></i></section>
           <section>余额</section>
--- src/uc/Routes/Home/Modules.jsx
+++ src/uc/Routes/Home/Modules.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router';
+import rczp from '../../img/rczp.png';
+import kjfw from '../../img/kjfwlqt.png';
+import zb from '../../img/zb.png';
+import zc from '../../img/zc.png';
+import cqbh from '../../img/cqbh.png';
+import cygc from '../../img/cygc.png';
 
 export default function Modules() {
   const imgStyle = {
@@ -11,69 +17,69 @@ export default function Modules() {
   return (
     <div className="modulesWrap">
       <ul className="modules">
-        <li>
+        <li className="modules_item">
           <Link to="/rczp" className="module">
-            <figure style={figureStyle}>
-              <img style={imgStyle} src="http://img1.3lian.com/gif/more/11/201209/03e4eb4be19da810a4567d7f40e9f0c0.jpg" alt="人才招聘" />
-              <figcaption>人才招聘</figcaption>
+            <figure className="modules_item_fig" style={figureStyle}>
+              <div className="rczp"><img style={imgStyle} src={rczp} alt="人才招聘" /></div>
+              <figcaption className="modules_item_fig_cap">人才招聘</figcaption>
             </figure>
           </Link>
         </li>
-        <li>
+        <li className="modules_item">
           <Link to="/zc" className="module">
-            <figure style={figureStyle}>
-              <img style={imgStyle} src="http://img1.3lian.com/gif/more/11/201209/03e4eb4be19da810a4567d7f40e9f0c0.jpg" alt="众筹" />
-              <figcaption>众筹</figcaption>
+            <figure className="modules_item_fig" style={figureStyle}>
+              <div className="zc"><img style={imgStyle} src={zc} alt="众筹" /></div>
+              <figcaption className="modules_item_fig_cap">众筹</figcaption>
             </figure>
           </Link>
         </li>
-        <li>
+        <li className="modules_item">
           <Link to="/cysj" className="module">
-            <figure style={figureStyle}>
-              <img style={imgStyle} src="http://img1.3lian.com/gif/more/11/201209/03e4eb4be19da810a4567d7f40e9f0c0.jpg" alt="创意设计" />
-              <figcaption>创意设计</figcaption>
+            <figure className="modules_item_fig" style={figureStyle}>
+              <div className="cysj"><img style={imgStyle} src={kjfw} alt="创意设计" /></div>
+              <figcaption className="modules_item_fig_cap">创意设计</figcaption>
             </figure>
           </Link>
         </li>
-        <li>
+        <li className="modules_item">
           <Link to="/srdz" className="module">
-            <figure style={figureStyle}>
-              <img style={imgStyle} src="http://img1.3lian.com/gif/more/11/201209/03e4eb4be19da810a4567d7f40e9f0c0.jpg" alt="私人定制" />
-              <figcaption>私人定制</figcaption>
+            <figure className="modules_item_fig" style={figureStyle}>
+              <div className="srdz"><img style={imgStyle} src={cqbh} alt="私人定制" /></div>
+              <figcaption className="modules_item_fig_cap">私人定制</figcaption>
             </figure>
           </Link>
         </li>
       </ul>
       <ul className="modules">
-        <li>
+        <li className="modules_item">
           <Link to="/zb" className="module">
-            <figure style={figureStyle}>
-              <img style={imgStyle} src="http://img1.3lian.com/gif/more/11/201209/03e4eb4be19da810a4567d7f40e9f0c0.jpg" alt="众包" />
-              <figcaption>众包</figcaption>
+            <figure className="modules_item_fig" style={figureStyle}>
+              <div className="zb"><img style={imgStyle} src={zb} alt="众包" /></div>
+              <figcaption className="modules_item_fig_cap">众包</figcaption>
             </figure>
           </Link>
         </li>
-        <li>
+        <li className="modules_item">
           <Link to="/rczp" className="module">
-            <figure style={figureStyle}>
-              <img style={imgStyle} src="http://img1.3lian.com/gif/more/11/201209/03e4eb4be19da810a4567d7f40e9f0c0.jpg" alt="人才招聘" />
-              <figcaption>人才招聘</figcaption>
+            <figure className="modules_item_fig" style={figureStyle}>
+              <div className="cygc"><img style={imgStyle} src={cygc} alt="创意工厂" /></div>
+              <figcaption className="modules_item_fig_cap">创意工厂</figcaption>
             </figure>
           </Link>
         </li>
-        <li>
+        <li className="modules_item">
           <Link to="/rczp" className="module">
-            <figure style={figureStyle}>
-              <img style={imgStyle} src="http://img1.3lian.com/gif/more/11/201209/03e4eb4be19da810a4567d7f40e9f0c0.jpg" alt="人才招聘" />
-              <figcaption>人才招聘</figcaption>
+            <figure className="modules_item_fig" style={figureStyle}>
+              <div className="kjfw"><img style={imgStyle} src={kjfw} alt="会计服务" /></div>
+              <figcaption className="modules_item_fig_cap">会计服务</figcaption>
             </figure>
           </Link>
         </li>
-        <li>
+        <li className="modules_item">
           <Link to="/rczp" className="module">
-            <figure style={figureStyle}>
-              <img style={imgStyle} src="http://img1.3lian.com/gif/more/11/201209/03e4eb4be19da810a4567d7f40e9f0c0.jpg" alt="人才招聘" />
-              <figcaption>人才招聘</figcaption>
+            <figure className="modules_item_fig" style={figureStyle}>
+              <div className="cqbh"><img style={imgStyle} src={cqbh} alt="产权保护" /></div>
+              <figcaption className="modules_item_fig_cap">产权保护</figcaption>
             </figure>
           </Link>
         </li>
