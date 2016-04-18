0344581780f8c2fc52185b2b7a27267232521b34
lqt <1131763154@qq.com>
2016-04-18 09:47:39 +0800

    头部修改

--- src/common/js/showMenu.js
+++ src/common/js/showMenu.js
@@ -1,7 +1,9 @@
 import { $ } from 'func';
 
 export default function showMenu() {
-  $('.header_menu').addEventListener('click', () => {
+  const menu = document.querySelector('.header_menu');
+  if (!menu) return;
+  menu.addEventListener('click', () => {
     $('.header_menu_wrap').classList.toggle('is-show');
   });
 }
