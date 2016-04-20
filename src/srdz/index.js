fd9f09ddf4c2b52b88809b007eb420bd8b577cbe
lqt <1131763154@qq.com>
2016-04-20 14:01:07 +0800

    优化

--- src/srdz/index.js
+++ src/srdz/index.js
@@ -22,6 +22,8 @@ if (pathname.indexOf('detail') !== -1) {
   search();
 } else if (pathname.indexOf('list') !== -1) {
   list();
+  showMenu();
 } else if (pathname.indexOf('index') !== -1 || pathname.split('/').length < 4) {
   index();
+  showMenu();
 }
