cd87862706c4cfd5fd96af5e4987f40330e6c2dc
Victor <a-thok@outlook.com>
2016-04-22 17:40:47 +0800

    众筹 优化

--- src/zc/js/detail.js
+++ src/zc/js/detail.js
@@ -1,14 +1,17 @@
 import { $ } from 'func';
 
 export default function detail() {
-  const shareArry = [$('.header_link'), $('.dtImg_btn')];
+  const shareArry = [$('.header_link')[1], $('.dtImg_btn')];
   const share = $('.sliderShare');
+  const docCl = document.documentElement.classList;
   for (let i = 0, len = shareArry.length; i < len; i++) {
     shareArry[i].addEventListener('click', () => {
+      docCl.add('is-static');
       share.style.display = 'block';
     });
   }
-  $('.sliderShare_box_cencel').addEventListener('click', () => {
+  $('.sliderShare_box_cancel').addEventListener('click', () => {
+    docCl.remove('is-static');
     share.style.display = 'none';
   });
 }
