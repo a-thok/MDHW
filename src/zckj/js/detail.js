9b03ddf313b9c161d89d7b91adbec698ad7017d6
jixu <724921588@qq.com>
2016-04-25 17:38:29 +0800

    众创空间微信微博过滤

--- src/zckj/js/detail.js
+++ src/zckj/js/detail.js
@@ -5,4 +5,16 @@ import { $ } from 'func';
 export default function detail() {
   slider(document.querySelector('.sliderBox'));
   overflow($('.tagList'));
+
+  function showFilter(btn) {
+    btn.addEventListener('click', (e) => {
+      e.stopPropagation();
+      e.currentTarget.children[0].classList.toggle('is-filter');
+    });
+    document.body.addEventListener('click', () => {
+      btn.children[0].classList.remove('is-filter');
+    });
+  }
+  showFilter($('.ftCtrl_item')[0]);
+  showFilter($('.ftCtrl_item')[2]);
 }
