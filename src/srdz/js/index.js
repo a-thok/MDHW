b1ed60041bd0a05f6c531175da137137d0ecc1be
jixu <724921588@qq.com>
2016-04-25 11:43:29 +0800

    添加轮播图 JS

--- src/srdz/js/index.js
+++ src/srdz/js/index.js
@@ -1,7 +1,9 @@
 import { $ } from 'func';
+import slider from 'slider';
 import goToSearch from 'goToSearch';
 
 export default function index() {
+  slider($('.sliderBox'));
   goToSearch({
     input: $('.srch_input'),
     partialUrl: `http://${SRDZ_HOST}/m/home/search?kw=`,
