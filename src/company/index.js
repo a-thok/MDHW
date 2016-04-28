2c1d008bda67fd059c1fa0180c04763ad994e97e
jixu <724921588@qq.com>
2016-04-28 09:57:58 +0800

    更换公司黄页图片

--- src/company/index.js
+++ src/company/index.js
@@ -1,5 +1,15 @@
 import './css/main.css';
 
+import './img/company_about.png';
+import './img/company_slider.png';
+import './img/company_case.png';
+import './img/company_news.png';
+import './img/company_contact.png';
+import './img/company_kjdb.png';
+import './img/company_zbzc.png';
+import './img/company_cyzx.png';
+import './img/company_zlcy.png';
+
 // 解决click事件的移动端延迟
 import fastclick from 'fastclick';
 fastclick.attach(document.body);
