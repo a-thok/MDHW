10e86bc90c0cb080d048bf5eb706104681eddd61
lqt <1131763154@qq.com>
2016-05-05 08:27:59 +0800

    添加菜单栏

--- src/common/js/showMenu.js
+++ src/common/js/showMenu.js
@@ -2,6 +2,7 @@ import { $ } from 'func';
 
 export default function showMenu() {
   const menu = document.querySelector('.header_menu');
+
   if (!menu) return;
   menu.addEventListener('click', () => {
     $('.header_menu_wrap').classList.toggle('is-show');
--- src/uc/components/Header/index.jsx
+++ src/uc/components/Header/index.jsx
@@ -7,8 +7,17 @@ export default function Header(props) {
       <div className="header_sect">
         <Link className="header_link" style={{ visibility: props.to === false ? 'hidden' : 'visible' }} to={props.to || '/'}><i className="fa fa-angle-left"></i></Link>
       </div>
-      <div className="header_sect">
-        <a className="header_link" href="/main/Logout">退出</a>
+      <div className="header_sect header_sect-menu">
+        <span className="header_menu" onClick={ props.onToggleMenu} ><i></i><i></i><i></i></span>
+        <div className={ props.isShowMenu === false ? 'header_menu_wrap' : 'header_menu_wrap is-show' }>
+          <div className="arrow-wrap"><i className="arrow"></i></div>
+          <ul className="header_menu_list">
+            <li className="header_menu_list_item"><a className="header_link" href="#">梦高速首页</a></li>
+            <li className="header_menu_list_item"><a className="header_link" href="#">创客空间</a></li>
+            <li className="header_menu_list_item"><a className="header_link" href="#">关于我们</a></li>
+            <li className="header_menu_list_item"><a className="header_link" href="#">个人中心</a></li>
+          </ul>
+        </div>
       </div>
     </header>
   );
