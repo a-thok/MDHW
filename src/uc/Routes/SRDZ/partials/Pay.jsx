c9cc385cea4720a7707be8432f15c0b4b4b22418
lqt <1131763154@qq.com>
2016-05-16 14:54:33 +0800

    修改小bug

--- src/uc/Routes/SRDZ/partials/Pay.jsx
+++ src/uc/Routes/SRDZ/partials/Pay.jsx
@@ -10,7 +10,7 @@ export default function Pay(props) {
       credentials: 'include',
       body: JSON.stringify({
         payment,
-        number: this.props.number
+        number: props.number
       })
     });
   }
