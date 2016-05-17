289f7997c2cf05f2d9a25aebbe07979ac0f13762
jixu <724921588@qq.com>
2016-05-17 11:05:35 +0800

    添加代理

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ app.use('/m/login', proxy(url.parse('http://192.168.2.18:8085/m/login')));
 app.use('/m/reg', proxy(url.parse('http://192.168.2.18:8085/m/reg')));
 app.use('/m/Third', proxy(url.parse('http://192.168.2.18:8085/m/Third')));
 app.use('/m/Pwd', proxy(url.parse('http://192.168.2.18:8085/m/pwd')));
+app.use('/m/Com', proxy(url.parse('http://192.168.2.18:8085/m/Com')));
 
 app.listen(app.get('port'), (err) => {
   if (err) {
