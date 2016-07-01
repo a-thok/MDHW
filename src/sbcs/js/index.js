2b5306452b70ef5e8aebdcdbb6586cd4bd25445c
lqt <1131763154@qq.com>
2016-07-01 14:02:42 +0800

    首页js

--- src/sbcs/js/index.js
+++ src/sbcs/js/index.js
@@ -1,6 +1,12 @@
 import { $ } from 'func';
 import slider from 'slider';
+import goToSearch from 'goToSearch';
 
 export default function index() {
   slider($('.sliderBox'));
+  goToSearch({
+    input: $('.srch_input'),
+    partialUrl: `http://${ZB_HOST}/m/home/search?kw=`,
+    btn: $('.srch_btn')
+  });
 }
