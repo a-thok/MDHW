3993d8b52f85c7d750fec0110d73c240bff5865c
lqt <1131763154@qq.com>
2016-07-04 10:46:35 +0800

    公用过滤栏

---
+++ src/sbcs/js/selectMore.js
@@ -0,0 +1,48 @@
+import { $from, $parent } from 'func';
+import render from 'render';
+
+export function selectMore(config) {
+  // 多个条件选择过滤
+  $from('.filter_content_list').forEach(el => {
+    el.addEventListener('click', e => {
+      const cl = e.target.classList;
+      if (cl.contains('is-active')) return;
+      if ((e.target !== e.currentTarget) || cl.contains('grid_item_icon')) {
+        const wrapper = $parent(e.target, '.filter_item');
+
+        // 选中项视觉效果
+        const items = cl.contains('grid_item_icon') ? $from('.grid_item_icon') : $from(e.currentTarget.children);
+        items.forEach(_e => _e.classList.remove('is-active'));
+
+        let type; // 过滤值
+        let typeBig; // 过滤值
+        let text; // 填充文本
+        const filter = e.currentTarget.getAttribute('data-filter');
+        const filterBig = e.currentTarget.getAttribute('data-filterBig');
+        if (cl.contains('filter_content_list_item_more')) {
+          cl.add('is-active');
+          type = e.target.getAttribute('data-code');
+          typeBig = e.target.getAttribute('data-codeBig');
+          text = e.target.textContent.trim();
+          config.params.pageIndex = 0;
+          config.params[filter] = type;
+          config.params[filterBig] = typeBig;
+          config.immediate = true;
+          render(config);
+        } else {
+          cl.add('is-active');
+          type = e.target.getAttribute('data-code');
+          text = e.target.textContent.trim().slice(0, 3);
+          config.params.pageIndex = 0;
+          config.params[filter] = type;
+          config.immediate = true;
+          render(config);
+        }
+        wrapper.querySelector('.filter_active').textContent = text;
+        // 隐藏当前过滤器
+        wrapper.classList.remove('is-show');
+        document.documentElement.classList.remove('is-static'); // 恢复body滚动
+      }
+    });
+  });
+}
