98ea6a1fa92a026b6380c35089941f8e25c655af
lyl714525 <779549169@qq.com>
2016-07-08 08:33:30 +0800

    商标超市 详情页  立即购买

--- src/sbcs/js/detail.js
+++ src/sbcs/js/detail.js
@@ -5,4 +5,14 @@ import favorite from 'favorite';
 export default function detail() {
   favorite($('.ftCtrl_item')[0], 'id', '/m/RShop/Collect/CollectAdd', '/m/RShop/Collect/CollectDel');
   share($('.ftCtrl_item')[2]);
+
+  // 立即购买-弹出弹窗
+  const text = $('.pupop_bg');
+  $('.ftCtrl_item_btn').addEventListener('click', () => {
+    text.classList.add('is-show');
+  });
+  // 关闭弹窗
+  $('.close_pupop').addEventListener('click', () => {
+    text.classList.remove('is-show');
+  });
 }
