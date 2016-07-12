970365b2f065f3855d19c700036449f58b5a80ba
lqt <1131763154@qq.com>
2016-07-12 09:23:02 +0800

    购买并支付功能实现

--- src/sbcs/js/detail.js
+++ src/sbcs/js/detail.js
@@ -1,7 +1,7 @@
 import { $ } from 'func';
 import share from 'share';
 import favorite from 'favorite';
-import xhr from 'xhr';
+// import xhr from 'xhr';
 
 export default function detail() {
   favorite($('.ftCtrl_item')[0], 'id', '/m/RShop/Collect/CollectAdd', '/m/RShop/Collect/CollectDel');
@@ -22,11 +22,13 @@ export default function detail() {
     text.classList.remove('is-show');
     // const arr = location.pathname.split('/');
     // const id = arr[arr.length - 1];
-    const id = 1;
-    xhr('/m/RShop/Order/OrderConfirm', Object.assign({ id }), (res) => {
-      const str = encodeURIComponent(JSON.stringify(res.result));
-      document.cookie = `Sbcsdata=${str};path=/;domain=dreamhiway.com`;
-      // location.href = `http://${MAIN_HOST}/m/user#/Sbcs/order`; // 未登录情况下，转跳可能有问题
-    }, true);
+    const id = 4;
+    document.cookie = `SbcsID=${id};path=/;domain=dreamhiway.com`;
+    location.href = `http://${MAIN_HOST}/m/user#/Sbcs/order`;
+    // xhr('/m/RShop/Order/OrderConfirm', Object.assign({ id }), (res) => {
+    //   const str = encodeURIComponent(JSON.stringify(res.result));
+    //   document.cookie = `Sbcsdata=${str};path=/;domain=dreamhiway.com`;
+    //   location.href = `http://${MAIN_HOST}/m/user#/Sbcs/order`; // 未登录情况下，转跳可能有问题
+    // }, true);
   });
 }
--- src/uc/Routes/SBCS/index.jsx
+++ src/uc/Routes/SBCS/index.jsx
@@ -1,24 +1,121 @@
 import React, { Component } from 'react';
 import getPath from '../../mixins/getPath.js';
+// import { $cookie } from 'func';
 
 export default class Sbcs extends Component {
   constructor(props) {
     super(props);
     this.state = {
       Order: {
-        mode: 1
+        mode: 1,
+        data: {},
+        ret: {},
+        num: '',
+        text: ''
       },
       Pay: {
-        type: 0
+        data: {},
+        type: 'wapAlipay'
       }
     };
     this.changeInvoice = this.changeInvoice.bind(this);
     this.collapse = this.collapse.bind(this);
+    this.fetchOrder = this.fetchOrder.bind(this);
+    this.postOrder = this.postOrder.bind(this);
+    this.payClick = this.payClick.bind(this);
+    this.textChange = this.textChange.bind(this);
+  }
+  //  备注信息
+  textChange(value) {
+    const newState = this.state.Order;
+    newState.text = value;
+    this.setState({ Order: newState });
   }
   // 获取订单数据
-  // getData() {
-  //   const newState = Object.assign({}, this.state.Order);
-  // }
+  fetchOrder() {
+    fetch('/m/Sys/RShop/Order/OrderConfirm', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include',
+      body: JSON.stringify({ id: 4/* $cookie().SbcsID */ })
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.success) {
+          const newState = Object.assign({}, this.state);
+          newState.Order.data = res.result.data;
+          newState.Order.ret = res.result.ret;
+          newState.Pay.data = res.result.data;
+          this.setState(newState);
+        }
+      });
+  }
+  // 提交订单
+  postOrder() {
+    let receiveName;
+    let receivePhone;
+    let districtCode;
+    let receiveAddress;
+    if (this.props.orderAddr.province_name) {
+      const o = this.props.orderAddr;
+      receiveName = o.name;
+      receivePhone = o.mobile;
+      districtCode = o.district_code;
+      receiveAddress = `${o.province_name}省${o.city_name}市${o.district_name}${o.address}`;
+    } else {
+      const o = this.state.Order.ret;
+      receiveName = o.name;
+      receivePhone = o.mobile;
+      districtCode = o.district_code;
+      receiveAddress = `${o.province_name}省${o.city_name}市${o.district_name}${o.address}`;
+    }
+    fetch('/m/Sys/RShop/Order/ContractAdd', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include',
+      body: JSON.stringify({
+        receiveName,
+        receivePhone,
+        districtCode,
+        receiveAddress,
+        remark: this.state.Order.text,
+        ttid: 4/* $cookie().SbcsID */
+      })
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.success) {
+          const projectID = res.result.id;
+          fetch('/m/Sys/Rshop/OrderRshop1/Add', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            credentials: 'include',
+            body: JSON.stringify({
+              projectID
+            })
+          })
+            .then((res) => res.json())
+            .then((res) => {
+              if (res.success) {
+                const newState = this.state.Order;
+                newState.num = res.result.number;
+                this.setState({ Pay: newState });
+                window.location.hash = '#/sbcs/pay';
+              }
+            });
+        }
+      });
+  }
+  // 付款
+  payClick() {
+    location.href = `http://www.dreamhiway.com/m/Sys/Rshop/OrderRshop1/Alipay?payment=${this.state.Pay.type}&number=${this.state.Order.num}`;
+  }
   // 风险提示
   collapse() {
     const newState = this.state.Order;
@@ -45,15 +142,17 @@ export default class Sbcs extends Component {
       case 'Order':
         extra = {
           collapse: this.collapse,
-          orderAddr: this.props.orderAddr
-          // fetchOrder: this.fetchOrder.bind(this),
-          // toggleDetail: this.toggleDetail.bind(this)
+          postOrder: this.postOrder,
+          orderAddr: this.props.orderAddr,
+          textChange: this.textChange,
+          fetchOrder: this.fetchOrder
         };
         break;
       case 'Pay':
         extra = {
-          changeInvoice: this.changeInvoice
-          // toggleDetail: this.toggleDetail.bind(this)
+          changeInvoice: this.changeInvoice,
+          payClick: this.payClick,
+          fetchOrder: this.fetchOrder
         };
         break;
       default:
