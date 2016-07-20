b232f9efeb7b6b2af4e2d20965bd755853d17219
lqt <1131763154@qq.com>
2016-07-20 09:27:28 +0800

    提交商标

--- src/sbcs/js/submit.js
+++ src/sbcs/js/submit.js
@@ -35,4 +35,42 @@ export default function submit() {
         setTimeout(() => modal.classList.remove('is-show'), 2500);
       });
   });
+  // 图片上传
+  const file = document.getElementById('file');
+  const img = $('.submitImg_img');
+  file.addEventListener('change', (event) => {
+    const file = event.target.files[0];
+    // 图片预览
+    const fileReader = new FileReader();
+    fileReader.addEventListener('load', (event) => {
+      const url = event.target.result;
+      img.src = url;
+    });
+    fileReader.readAsDataURL(file);
+
+
+    // const canvas = document.createElement('canvas');
+    // canvas.setAttribute('width', 530);
+    // canvas.setAttribute('height', 270);
+    // const ctx = canvas.getContext('2d');
+
+    // ctx.drawImage(img, 0, 0, 530, 270);
+    // const fileBlob = canvas.toBlob('image/jpeg', 0.7);
+
+
+    // const formData = new FormData();
+    // formData.append('file', fileBlob);
+    // fetch('/api', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'multipart/form-data'
+    //   },
+    //   credential: 'include',
+    //   body: formData
+    // })
+    //   .then(res => res.json())
+    //   .then(data => {
+    //     params.url = data.path;
+    //   });
+  });
 }
